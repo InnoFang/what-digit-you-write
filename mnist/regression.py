9ff50720b16af0a7117225ee0dfd933127d3d11f
InnoFang <innofang@outlook.com>
2018-10-11 12:36:19 +0800

    :art: :bug: optimize and fix bug

--- mnist/regression.py
+++ mnist/regression.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 import tensorflow as tf
 from mnist.load_data import mnist
 
@@ -14,9 +18,16 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
 
+saver = tf.train.Saver()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for _ in range(1000):
         batch = mnist.train.next_batch(50)
         train_step.run(feed_dict={x: batch[0], y_: batch[1]})
-    print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+    save_path = saver.save(sess,
+                           os.path.join(os.path.dirname(__file__), 'model', 'regression.ckpt'),
+                           write_meta_graph=False, write_state=False)
+
+    print('Saved: ', save_path)
