eeb2734fbcc2d0811ab70567796751e6267f1f44
InnoFang <innofang@outlook.com>
2018-10-12 14:30:43 +0800

    :lipstick: implement a simple writing board by using canvas

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,54 @@
 window.onload = () => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+    const lineWidth = 10;
+    const lineColor = "#000";
+    const canvasWidth = 561; // 20 * 28 + 1
+    const canvasHeight = 561; // 20 * 28 + 1
 
-    canvas.width = 561; // 20 * 28 + 1
-    canvas.height = 561; // 20 * 28 + 1
-}
+    let isDrawing = false;
+    let curPos; // current position
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+
+    function getPosition(clientX, clientY) {
+        let box = canvas.getBoundingClientRect();
+        return {x: clientX - box.x, y: clientY - box.y}
+    }
+
+    function draw(e) {
+        if (isDrawing) {
+            let pos = getPosition(e.clientX, e.clientY);
+            ctx.strokeStyle = lineColor;
+            ctx.lineWidth = lineWidth;
+            ctx.lineCap = "round";
+            ctx.lineJoin = "round";
+            ctx.beginPath();
+            ctx.moveTo(curPos.x, curPos.y);
+            ctx.lineTo(pos.x, pos.y);
+            ctx.stroke()
+            ctx.closePath();
+            curPos = pos;
+        }
+    }
+
+    function clear() {
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    }
+
+    canvas.onmousedown = function (e) {
+        isDrawing = true;
+        curPos = getPosition(e.clientX, e.clientY);
+        draw(e);
+    };
+
+    canvas.onmousemove = function (e) {
+        draw(e);
+    };
+
+    canvas.onmouseup = function (e) {
+        isDrawing = false;
+    }
+};
