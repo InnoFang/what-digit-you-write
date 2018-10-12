7a7adcf0d500d7e90116aca12bdd92817821e64f
InnoFang <innofang@outlook.com>
2018-10-12 13:51:10 +0800

    :lipstick: Update UI

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,7 @@
-const canvas = document.getElementById("canvas");``
-const ctx = document.getElementById("2d");``
-
 window.onload = () => {
-    canvas.width  = 449; // 16 * 28 + 1
-    canvas.height = 449; // 16 * 28 + 1
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 561; // 20 * 28 + 1
+    canvas.height = 561; // 20 * 28 + 1
 }
