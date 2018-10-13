713094af767546fbb6ae3cde6fac9a7486c846e4
InnoFang <innofang@outlook.com>
2018-10-13 14:23:55 +0800

    :art: :fire: optimize and clean

--- mnist/cnn.py
+++ mnist/cnn.py
@@ -1,39 +1,9 @@
 import os
 import tensorflow as tf
 from mnist.input import mnist
+from mnist import model
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape=shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(value=0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(
-        input=x,
-        filter=W,
-        strides=[1, 1, 1, 1],
-        padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(
-        value=x,
-        ksize=[1, 2, 2, 1],
-        strides=[1, 2, 2, 1],
-        padding='SAME')
-
-
-x = tf.placeholder(tf.float32, shape=[None, 784])
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
-
 
 """
 Defining the Constant
@@ -43,43 +13,10 @@ n_iterations = 2000
 batch_size = 50
 dropout = 0.5
 
-
-"""
-Building the TensorFlow Graph
-"""
-# input layer
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-x_image = tf.reshape(x, [-1, 28, 28, 1])  # 28x28 gray image
-
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-# hidden layer
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-# full-connection
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-# dropout
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
 keep_prob = tf.placeholder(dtype=tf.float32)
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-# output layer
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-
+y_conv, variables = model.convolutional(x, keep_prob)
 
 """
 Training and Testing
@@ -89,7 +26,7 @@ train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
 
-saver = tf.train.Saver()
+saver = tf.train.Saver(variables)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(n_iterations):
--- mnist/regression.py
+++ mnist/regression.py
@@ -1,27 +1,34 @@
 import os
 import tensorflow as tf
 from mnist.input import mnist
+from mnist import model
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
+"""
+Defining the Constant
+"""
+n_iterations = 2000
+batch_size = 50
+
+
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
+y, variables = model.regression(x)
 
-W = tf.Variable(tf.zeros([784, 10]), dtype=tf.float32, name="weights")
-b = tf.Variable(tf.zeros([10]), dtype=tf.float32, name="biases")
-y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-# train
+"""
+Training and Testing
+"""
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
 
-saver = tf.train.Saver()
+saver = tf.train.Saver(variables)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for _ in range(1000):
-        batch = mnist.train.next_batch(50)
+    for _ in range(n_iterations):
+        batch = mnist.train.next_batch(batch_size)
         train_step.run(feed_dict={x: batch[0], y_: batch[1]})
     print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
