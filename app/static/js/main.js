1d4dec21033e5613cfd69f3a8e7d65b82ca765f2
InnoFang <innofang@outlook.com>
2018-10-16 13:41:07 +0800

    :lipstick: :sparkles: :art: :bug: Update UI and implement the sub-image generation and fix some bugs

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -18,6 +18,7 @@ window.onload = () => {
     picture.width = pictureWidth;
     picture.height = pictureHeight;
 
+    clear();
 
     function getPosition(clientX, clientY) {
         let box = canvas.getBoundingClientRect();
@@ -34,7 +35,7 @@ window.onload = () => {
             ctx.beginPath();
             ctx.moveTo(curPos.x, curPos.y);
             ctx.lineTo(pos.x, pos.y);
-            ctx.stroke()
+            ctx.stroke();
             ctx.closePath();
             curPos = pos;
         }
@@ -52,43 +53,14 @@ window.onload = () => {
 
     canvas.onmouseup = function (e) {
         isDrawing = false;
-        // let img = new Image();
-        //
-        // img.onload = function () {
-        //
-        //     picCtx.drawImage(img, 0, 0, picture.width, picture.height);
-        //     const imgData = picCtx.getImageData(0, 0, picture.width, picture.height).data;
-        //     let inputs = [];
-        //     let total = 0;
-        //     console.log(imgData);
-        //     for (let i = 0; i < picture.width; ++i) {
-        //         for (let j = 0; j < picture.height; ++j) {
-        //             const idx = (i * picture.width + j) * 4;
-        //             const r = imgData[idx];
-        //             const g = imgData[idx + 1];
-        //             const b = imgData[idx + 2];
-        //             // const gray = 0.299 * r + 0.587 * g + 0.114 * b;
-        //             const gray = (r + g + b) / 3;
-        //             inputs[i * 28 + j] = gray;
-        //             total += gray;
-        //             // imgData[idx] = imgData[idx + 1] = imgData[idx + 2] = gray;
-        //             // imgData[idx + 3] = 255;
-        //             // console.log("r: " + r + ", g: " + g + ", b: " + b + ", gray: " + gray)
-        //             // picCtx.fillStyle = 'rgb(' + [0.299 * r, 0.587 * g, 0.114 * b].join(',') + ')';
-        //             // picCtx.fillRect(i * 5, j * 5, 5, 5);
-        //         }
-        //     }
-        //     // picCtx.putImageData(imgData, 0, 0);
-        //     console.log(total);
-        //
-        // };
+
         const img = new Image();
+        img.src = canvas.toDataURL();
         img.onload = function () {
             let inputs = [];
-            let total = 0;
-            const small = document.createElement("canvas").getContext('2d');
-            picCtx.drawImage(img, 0, 0, img.width, img.height, 0, 0, 28, 28);
-            let data = picCtx.getImageData(0, 0, 28, 28).data;
+            const input = document.createElement("canvas").getContext('2d');
+            input.drawImage(img, 0, 0, img.width, img.height, 0, 0, 28, 28);
+            let data = input.getImageData(0, 0, 28, 28).data;
             for (let i = 0; i < 28; ++i) {
                 for (let j = 0; j < 28; ++j) {
                     let px = 4 * (i * 28 + j);
@@ -96,18 +68,19 @@ window.onload = () => {
                     let g = data[px + 1];
                     let b = data[px + 2];
                     inputs[i * 28 + j] = (r + g + b) / 3;
-                    total += (r + g + b) / 3;
-                    // picCtx.fillStyle = 'rgb(' + [r, g, b].join(',') + ")";
-                    // picCtx.fillRect(i * 5, j * 5, 5, 5)
+                    picCtx.fillStyle = 'rgb(' + [r, g, b].join(',') + ")";
+                    picCtx.fillRect(j * 5, i * 5, 5, 5)
                 }
             }
-            console.log(total);
         };
-        img.src = canvas.toDataURL();
     };
 
-    reset.onclick = function () {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        picCtx.clearRect(0, 0, picture.width, picture.height);
+    reset.onclick = clear;
+
+    function clear() {
+        ctx.fillStyle = "#ffffff";
+        picCtx.fillStyle = "#ffffff";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        picCtx.fillRect(0, 0, picture.width, picture.height);
     }
 };
