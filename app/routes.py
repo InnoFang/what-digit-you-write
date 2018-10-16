d03c92b38864dd95e218fe02a6f327b5dfc13207
InnoFang <innofang@outlook.com>
2018-10-16 13:44:02 +0800

    :art: update API

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from app import app
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/input')
+def input():
+    print(request.json)
