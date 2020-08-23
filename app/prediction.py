8cac61f9b887b688abf75a8065ae9c005af83aff
InnoFang <innofang@outlook.com>
2020-08-23 19:31:43 +0800

    :art: update prediction and routes

--- app/prediction.py
+++ app/prediction.py
@@ -1,27 +1,11 @@
-from app.models.convolutional import LeNet5
-from app.models.regresssion import LinearRegression
-import os
-
-models = {}
+import tensorflow as tf
 
 
 def predict_by_regression(x_predict):
-    if 'regression' not in models:
-        model = LinearRegression()
-        checkpoint_save_path = 'models/checkpoint/Regression.ckpt'
-        if os.path.exists(checkpoint_save_path + '.index'):
-            model.load_weights(checkpoint_save_path)
-        models['regression'] = model
-
-    return models['regression'].predict(x_predict).flatten().tolist()
+    model = tf.keras.models.load_model('app/models/regression.h5')
+    return model.predict(x_predict).flatten().tolist()
 
 
 def predict_by_cnn(x_predict):
-    if 'convolutional' not in models:
-        model = LeNet5()
-        checkpoint_save_path = 'models/checkpoint/LeNet5.ckpt'
-        if os.path.exists(checkpoint_save_path + '.index'):
-            model.load_weights(checkpoint_save_path)
-        models['convolutional'] = model
-
-    return models['convolutional'].predict(x_predict).flatten().tolist()
+    model = tf.keras.models.load_model('app/models/convolutional.h5')
+    return model.predict(x_predict).flatten().tolist()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import numpy as np
 from app import app
+from PIL import Image
 from flask import render_template, request, jsonify
 from app.prediction import predict_by_regression, predict_by_cnn
 
@@ -11,8 +12,9 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # normalized - normalize the data from 0 to 255 to 0 to 1.
-    input_data = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0)
-    result_of_regression = predict_by_regression(input_data.reshape(1, 784))
+    # normalized
+    input_data = ((255 - np.array(request.json)) / 255.0)
+
+    result_of_regression = predict_by_regression(input_data.reshape(1, 28, 28))
     result_of_convolutional = predict_by_cnn(input_data.reshape(1, 28, 28, 1))
     return jsonify(data=[result_of_regression, result_of_convolutional])
