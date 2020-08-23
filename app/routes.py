1468bfa4ce654541017c8bbc6e9a52e5a50ac388
InnoFang <innofang@outlook.com>
2020-08-23 15:39:56 +0800

    :art: update routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, jsonify
 import numpy as np
 from app import app
-from app.prediction import regression_predict, cnn_predict
+from flask import render_template, request, jsonify
+from app.prediction import predict_by_regression, predict_by_cnn
 
 
 @app.route('/', methods=['GET'])
@@ -12,7 +12,7 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     # normalized - normalize the data from 0 to 255 to 0 to 1.
-    input_data = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-    result_of_regression = regression_predict(input_data)
-    result_of_cnn = cnn_predict(input_data)
-    return jsonify(data=[result_of_regression, result_of_cnn])
+    input_data = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0)
+    result_of_regression = predict_by_regression(input_data.reshape(1, 784))
+    result_of_convolutional = predict_by_cnn(input_data.reshape(1, 28, 28, 1))
+    return jsonify(data=[result_of_regression, result_of_convolutional])
