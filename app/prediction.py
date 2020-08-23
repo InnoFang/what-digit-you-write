d7e198c1363ee275e30aa4f3049b88e265de78ca
InnoFang <innofang@outlook.com>
2020-08-23 15:55:23 +0800

    :art: update prediction

--- app/prediction.py
+++ app/prediction.py
@@ -2,22 +2,26 @@ from app.models.convolutional import LeNet5
 from app.models.regresssion import LinearRegression
 import os
 
+models = {}
+
 
 def predict_by_regression(x_predict):
-    model = LinearRegression()
+    if 'regression' not in models:
+        model = LinearRegression()
+        checkpoint_save_path = 'models/checkpoint/Regression.ckpt'
+        if os.path.exists(checkpoint_save_path + '.index'):
+            model.load_weights(checkpoint_save_path)
+        models['regression'] = model
 
-    checkpoint_save_path = 'models/checkpoint/Regression.ckpt'
-    if os.path.exists(checkpoint_save_path + '.index'):
-        model.load_weights(checkpoint_save_path)
-
-    return model.predict(x_predict).flatten().tolist()
+    return models['regression'].predict(x_predict).flatten().tolist()
 
 
 def predict_by_cnn(x_predict):
-    model = LeNet5()
+    if 'convolutional' not in models:
+        model = LeNet5()
+        checkpoint_save_path = 'models/checkpoint/LeNet5.ckpt'
+        if os.path.exists(checkpoint_save_path + '.index'):
+            model.load_weights(checkpoint_save_path)
+        models['convolutional'] = model
 
-    checkpoint_save_path = 'models/checkpoint/LeNet5.ckpt'
-    if os.path.exists(checkpoint_save_path + '.index'):
-        model.load_weights(checkpoint_save_path)
-
-    return model.predict(x_predict).flatten().tolist()
+    return models['convolutional'].predict(x_predict).flatten().tolist()
