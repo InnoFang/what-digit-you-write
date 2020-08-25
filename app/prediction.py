cf4fc437df320126fc2de478b29568bbfb6805d7
InnoFang <innofang@outlook.com>
2020-08-25 09:03:09 +0800

    :art: optimize

--- app/prediction.py
+++ app/prediction.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 
 
 def predict_by_regression(x_predict):
+    x_predict = x_predict.reshape(1, 28, 28)
     model = tf.keras.models.load_model('app/models/regression.h5')
     return model.predict(x_predict).flatten().tolist()
 
 
 def predict_by_cnn(x_predict):
+    x_predict = x_predict.reshape(1, 28, 28, 1)
     model = tf.keras.models.load_model('app/models/convolutional.h5')
     return model.predict(x_predict).flatten().tolist()
--- app/routes.py
+++ app/routes.py
@@ -14,6 +14,6 @@ def predict():
     # normalized
     input_data = ((255 - np.array(request.json)) / 255.0)
 
-    result_of_regression = predict_by_regression(input_data.reshape(1, 28, 28))
-    result_of_convolutional = predict_by_cnn(input_data.reshape(1, 28, 28, 1))
+    result_of_regression = predict_by_regression(input_data)
+    result_of_convolutional = predict_by_cnn(input_data)
     return jsonify(data=[result_of_regression, result_of_convolutional])
