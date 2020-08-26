f049d6e74cb6664d23a2a9a8fa36a4bb0d25ecd3
InnoFang <innofang@outlook.com>
2020-08-26 13:01:58 +0800

    :art: optimize the 'convolutional' code

--- app/models/convolutional.py
+++ app/models/convolutional.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+import matplotlib.pyplot as plt
 import os
 
 if __name__ == '__main__':
@@ -9,13 +10,14 @@ if __name__ == '__main__':
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
     model = tf.keras.models.Sequential([
-        Conv2D(filters=6, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)),
-        MaxPool2D(pool_size=(2, 2)),
-        Conv2D(filters=16, kernel_size=(5, 5), activation='relu'),
-        MaxPool2D(pool_size=(2, 2)),
+        Conv2D(filters=16, kernel_size=(5, 5), padding='same', activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Conv2D(filters=36, kernel_size=(5, 5), padding='same', activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.25),
         Flatten(),
-        Dense(120, activation='relu'),
-        Dense(84, activation='relu'),
+        Dense(128, activation='relu'),
+        Dropout(0.5),
         Dense(10, activation='softmax')
     ])
 
@@ -23,7 +25,7 @@ if __name__ == '__main__':
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
                   metrics=['sparse_categorical_accuracy'])
 
-    checkpoint_save_path = 'checkpoint/LeNet5.ckpt'
+    checkpoint_save_path = 'checkpoint/convolutional.ckpt'
     if os.path.exists(checkpoint_save_path + '.index'):
         model.load_weights(checkpoint_save_path)
 
@@ -31,9 +33,9 @@ if __name__ == '__main__':
                                                      save_weights_only=True,
                                                      save_best_only=True)
 
-    model.fit(x_train, y_train,
+    history = model.fit(x_train, y_train,
               batch_size=128,
-              epochs=5,
+              epochs=20,
               validation_data=(x_test, y_test),
               validation_freq=1,
               callbacks=[cp_callback])
@@ -41,3 +43,21 @@ if __name__ == '__main__':
     model.save('convolutional.h5')
 
     model.summary()
+
+    acc = history.history['sparse_categorical_accuracy']
+    val_acc = history.history['val_sparse_categorical_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    plt.subplot(1, 2, 1)
+    plt.plot(acc, label='Training Accuracy')
+    plt.plot(val_acc, label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(loss, label='Training Loss')
+    plt.plot(val_loss, label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+    plt.show()
