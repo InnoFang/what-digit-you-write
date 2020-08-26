8051195352b749e9a103496f474e892cc289c663
InnoFang <innofang@outlook.com>
2020-08-26 13:29:00 +0800

    :art: add some annotation

--- app/predictors.py
+++ app/predictors.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import load_model
 class RegressionPredictor(object):
     __model = None
 
+    # ensure predictor be initialized only once by using Singleton Pattern
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '__instance'):
             cls.__instance = super().__new__(cls)
@@ -14,6 +15,7 @@ class RegressionPredictor(object):
     def predict(input_data):
         assert RegressionPredictor.__model, \
             "Use 'RegressionPredictor()' to initialize before using 'RegressionPredictor.predict()'"
+
         x = input_data.reshape(1, 28, 28)
         return RegressionPredictor.__model.predict(x).flatten().tolist()
 
@@ -21,6 +23,7 @@ class RegressionPredictor(object):
 class CNNPredictor(object):
     __model = None
 
+    # ensure predictor be initialized only once by using Singleton Pattern
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, '__instance'):
             cls.__instance = super().__new__(cls)
@@ -30,5 +33,6 @@ class CNNPredictor(object):
     @staticmethod
     def predict(input_data):
         assert CNNPredictor.__model, "Use 'CNNPredictor()' to initialize before using 'CNNPredictor.predict()'"
+
         x = input_data.reshape(1, 28, 28, 1)
         return CNNPredictor.__model.predict(x).flatten().tolist()
